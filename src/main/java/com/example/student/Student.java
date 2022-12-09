4f9a9efb1ab6e6d1e89469eb6a6be3b4a6930051
LiewShanWei <31199875+liewshanwei@users.noreply.github.com>
2022-12-09 16:15:16 +0800

    Added default constructor and setters

--- src/main/java/com/example/student/Student.java
+++ src/main/java/com/example/student/Student.java
@@ -6,18 +6,21 @@ import javax.persistence.Id;
 @Entity
 public class Student {
     @Id
-    private final String id;
-    private final String name;
+    private String id;
+    private String name;
+
+    public Student() {}
 
     public Student(String id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    public void setId(String id) { this.id = id; }
     public String getId() {
         return this.id;
     }
-
+    public void setName(String name) { this.name = name; }
     public String getName() {
         return this.name;
     }
